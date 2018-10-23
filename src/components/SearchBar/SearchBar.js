bdc19299828f4a0170319311d4eb0a14f4261269
Scott R. McCormick <Scott.McCormick@silabs.com>
2018-10-23 07:08:56 -0500

    refactor search submit as form to follow best practices

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,34 +4,31 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       term: ''
     };
-  }
-  search() {
-    this.props.onSearch(this.state.term);
   }
   handleTermChange(event) {
     this.setState({
       term: event.target.value
     })
   }
-  handleKeyPress(e) {
-    if (e.key === 'Enter') {
-      this.search()
-    }
+  handleSubmit(e) {
+    this.props.onSearch(this.state.term);
+    e.preventDefault();
   }
   render() {
     return (
       <div className="SearchBar">
-        <input onChange={this.handleTermChange} onKeyPress={this.handleKeyPress} placeholder="Enter A Song, Album, or Artist" />
-        <a onClick={this.search}>SEARCH</a>
+        <form onSubmit={this.handleSubmit} className="SearchBar">
+          <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+          <input type="submit" value="SEARCH" />
+        </form>
       </div>
     );
-  }  
+  }
 }
 
 export default SearchBar;
